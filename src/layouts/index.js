696b90f8aa4d3fd76dc67ab91671e2c9713b0c17
Noland <noland.germain@gmail.com>
2018-02-24 15:12:41 -0800

    Initial commit

---
+++ src/layouts/index.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+
+import './reset.scss'
+import './main.scss'
+
+class TemplateWrapper extends Component {
+  componentDidMount = () => {
+    console.log('hi');
+
+    window.Tawk_API = window.Tawk_API||{}
+    window.Tawk_LoadStart = new Date();
+
+    const s1=document.createElement("script")
+    const s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5a91eb9bd7591465c707fc25/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+  }
+
+  render(){
+    return(
+      <div>
+        <Helmet
+          title="Advanced Live Math Help"
+          meta={[
+            { name: 'description', content: 'Advanced university and college live math help chat' },
+            { name: 'keywords', content: 'math, live, chat, university, college, advanced' },
+          ]}
+        />
+        <Helmet>
+          {/* <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet" /> */}
+          <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet" />
+        </Helmet>
+        <div>
+          {this.props.children()}
+        </div>
+      </div>
+    );
+  }
+}
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
---
+++ src/pages/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <header id="site-header">
+      <div className="overlay" />
+      <h1>
+        <div className="sub">Ana's</div>
+        Advanced Math&nbsp;Help
+        <div className="sub">Live 7 days/week</div>
+      </h1>
+      <p>
+        Get live help with your college/university assignments and courses 7 days a week.
+        <br /><br />
+        Chat with me now for an instant quote!
+      </p>
+    </header>
+
+    <div id="site-content">
+      <h3>Hello and welcome!</h3>
+
+      <p>
+        My name is Ana and I love solving math problems and helping students with their assignments and understanding math concepts.
+      </p>
+
+      <p>
+        I hold a Masters Degree in Mathematics from the University of Alberta, Canada.
+        Chat with me about your math challenge today. I look forward to helping you out!
+      </p>
+
+      <h3>Rates</h3>
+      <p>
+        Rates vary from $40-60/hour depending on the type of math instruction.
+        We will first discuss your problem together and I will provide a quote.
+        Assignment help is custom quoted instantly&mdash;<strong>just chat with me or call 604-618-7227 to discuss</strong>.
+      </p>
+    </div>
+
+    <footer id="site-footer">
+      <div>
+        &copy; Copyright 2018 Anastasiya Lukyanova, all rights reserved
+      </div>
+      <div>
+        <a href="mailto:Ana@AdvancedMathHelp.live">Ana@AdvancedMathHelp.live</a>
+      </div>
+    </footer>
+
+  </div>
+)
+
+export default IndexPage
