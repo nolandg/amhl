3c86b8f669fc55a7f12f1fbece29da2c3f8b910e
Noland <noland.germain@gmail.com>
2018-02-24 20:27:37 -0800

    Add compression

--- scripts/pm2_start.sh
+++ scripts/pm2_start.sh
@@ -1,2 +1,2 @@
 cd /var/www/development/amhl
-sudo pm2 start http-server --name math-help -x -- -p 1908 -d false --gzip true
+sudo pm2 start http-server --name math-help -x -- -p 1908 -d False --gzip
